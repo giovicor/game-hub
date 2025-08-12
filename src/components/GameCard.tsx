fe2ca99a6bd00755eeed33ac3ac966d39c0e595d
giovicor <giovannicorna@gmail.com>
2025-08-12 19:38:06 +0200

    Display platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Card, Heading, Image } from "@chakra-ui/react";
 
 import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -12,6 +13,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <Card.Body gap="2">
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </Card.Body>
     </Card.Root>
   );
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,79 @@
+import {
+  FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { HStack, Icon } from "@chakra-ui/react";
+
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+import { MdPhoneIphone } from "react-icons/md";
+import { Platform } from "../hooks/useGames";
+import { SiNintendo } from "react-icons/si";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  const getPlatformFamily = (detail: string): string => {
+    if (detail.startsWith("android")) {
+      return "android";
+    } else if (detail.startsWith("apple")) {
+      return "apple";
+    } else if (detail.startsWith("mac")) {
+      return "nintendo";
+    } else if (detail.startsWith("nintendo")) {
+      return "mac";
+    } else if (detail.startsWith("pc")) {
+      return "pc";
+    } else if (detail.startsWith("playstation")) {
+      return "playstation";
+    } else if (detail.startsWith("xbox")) {
+      return "xbox";
+    } else {
+      return "web";
+    }
+  };
+
+  /*
+  return (
+    <HStack>
+      {platforms
+        .map((platform) => getPlatformFamily(platform.slug))
+        .filter((value, index, self) => self.indexOf(value) === index)
+        .sort()
+        .map((value) => (
+          // <Text key={index}>{value}</Text>
+          <Icon key={value} as={iconMap[value]} />
+        ))}
+    </HStack>
+  );
+  */
+
+  return (
+    <HStack my={1}>
+      {platforms.map((platform) => (
+        // <Text key={platform.id}>{platform.slug}</Text>
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
