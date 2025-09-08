0079ca83a401be333bac9fa900d87077a7e82ee8
giovicor <giovannicorna@gmail.com>
2025-09-09 00:38:01 +0200

    Customize the theme to get darker grays

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,21 @@ import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
 const customConfig = defineConfig({
   theme: {
     tokens: {
-      colors: {},
+      colors: {
+        gray: {
+          50: { value: "#f9f9f9" },
+          100: { value: "#ededed" },
+          200: { value: "#d3d3d3" },
+          300: { value: "#b3b3b3" },
+          400: { value: "#a0a0a0" },
+          500: { value: "#898989" },
+          600: { value: "#6c6c6c" },
+          700: { value: "#202020" },
+          800: { value: "#141414" },
+          900: { value: "#121212" },
+          950: { value: "#111111" },
+        },
+      },
     },
   },
 });
