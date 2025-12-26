224acba2dd067dd32858843c6d68543491b12181
giovicor <giovannicorna@gmail.com>
2025-12-26 18:59:30 +0100

    Fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,16 @@
 import { HStack, Image } from "@chakra-ui/react";
 
 import ColorModeSwitch from "./ColorModeSwitch";
+import { NavLink } from "react-router";
 import SearchInput from "./SearchInput";
 import logo from "../assets/logo.webp";
 
 const NavBar = () => {
   return (
     <HStack p={5}>
-      <Image src={logo} w="60px" />
+      <NavLink to="/">
+        <Image src={logo} w="60px" objectFit="cover" />
+      </NavLink>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,10 +2,12 @@ import { Input, InputGroup } from "@chakra-ui/react";
 
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router";
 import { useRef } from "react";
 
 const SearchInput = () => {
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
 
   const searchRef = useRef<HTMLInputElement>(null);
 
@@ -15,6 +17,7 @@ const SearchInput = () => {
         event.preventDefault();
         if (searchRef.current) {
           setSearchText(searchRef.current.value);
+          navigate("/");
         }
       }}
     >
