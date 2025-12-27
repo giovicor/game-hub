1785b9d2197bf405ebf150b784371e5c393a834e
giovicor <giovannicorna@gmail.com>
2025-12-27 16:30:31 +0100

    Fix chunk size warning limit.

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // plugins: [react(), tsconfigPaths()],
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 800,
+  },
 });
